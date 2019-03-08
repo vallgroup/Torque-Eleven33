fe6b7d2ab40cb4585423f476758a19ca1692a4ed
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-08 15:11:19 -0600

    hide unecessary admin pages

--- src/functions.php
+++ src/functions.php
@@ -59,10 +59,10 @@ if ( class_exists( 'Torque_Floor_Plans_Data_Source' ) ) {
  function torque_remove_menus(){
 
    //remove_menu_page( 'index.php' );                  //Dashboard
-   //remove_menu_page( 'edit.php' );                   //Posts
+   remove_menu_page( 'edit.php' );                   //Posts
    //remove_menu_page( 'upload.php' );                 //Media
    //remove_menu_page( 'edit.php?post_type=page' );    //Pages
-   //remove_menu_page( 'edit-comments.php' );          //Comments
+   remove_menu_page( 'edit-comments.php' );          //Comments
    //remove_menu_page( 'themes.php' );                 //Appearance
    //remove_menu_page( 'plugins.php' );                //Plugins
    //remove_menu_page( 'users.php' );                  //Users
