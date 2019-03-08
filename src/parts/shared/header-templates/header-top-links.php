36f129ae1fe868f0aedc3c554edb2ae8d17f937f
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-07 18:20:12 -0600

    add leasing number to header

--- src/parts/shared/header-templates/header-top-links.php
+++ src/parts/shared/header-templates/header-top-links.php
@@ -1,7 +1,13 @@
+<?php
+$leasing = get_field('leasing_phone_number', 'options');
+?>
+
 <div class="top-links">
-  <div class="top-link leasing">
-    Leasing
-  </div>
+  <?php if ($leasing) { ?>
+    <div class="top-link leasing">
+      Leasing <?php echo $leasing; ?>
+    </div>
+  <?php } ?>
   <div class="top-link">
     <a href="/">
       Resident Login
