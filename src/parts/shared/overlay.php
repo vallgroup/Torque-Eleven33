32b256c720054009f6cee997ef51f41d63d09c8c
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-15 11:15:50 -0600

    threes are svg image, not type

--- src/parts/shared/overlay.php
+++ src/parts/shared/overlay.php
@@ -1,8 +1,7 @@
 <?php if (($overlay_type === 'text' && $text_overlay) || ($overlay_type === 'default')) { ?>
   <div class="overlay type-<?php echo $overlay_type; ?>" >
       <?php if ($overlay_type === 'default') { ?>
-        <h1 class="hero-three">3</h1>
-        <h1 class="hero-three three-small">3</h1>
+        <div class="threes-image" ></div>
       <?php } else { ?>
         <h1>
           <?php echo $text_overlay; ?>
