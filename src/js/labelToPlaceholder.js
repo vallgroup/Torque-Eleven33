19ccfcd4ed91681d34a09db62976378716e21670
Mario Vallejo <mario@vallgroup.com>
2019-03-29 10:44:14 -0500

    add margin to header when logged in so the admin bar does not obstruct the header

--- src/js/labelToPlaceholder.js
+++ src/js/labelToPlaceholder.js
@@ -1,5 +1,10 @@
 ($ => {
   $(document).ready(() => {
+    console.log('ran')
+    const adminBar = $('#wpadminbar');
+    if (0 < adminBar.length) {
+      $('header.torque-header').css('margin-top', adminBar.height() )
+    }
     $(".grunion-field-wrap").each(function() {
       const label = $(this)
         .find("label")
